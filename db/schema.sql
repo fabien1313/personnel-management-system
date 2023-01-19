afd3206c66474a26aa5ea628fe9f7d8e8e1665fd
fabien moreno <fabienmoreno1331@yahoo.com>
2023-01-18 18:41:31 -0600

    role table

--- db/schema.sql
+++ db/schema.sql
@@ -8,4 +8,14 @@ CREATE TABLE department (
     PRIMARY KEY (id)
 );
 
+CREATE TABLE role (
+    id INT AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DEC NOT NULL,
+    department_id INT,
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON DELETE SET NULL
+);
+
 
