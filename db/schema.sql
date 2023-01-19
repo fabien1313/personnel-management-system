7ee5f211dc581280ddcf0db6fa46ef5cd8f0dbfd
fabien moreno <fabienmoreno1331@yahoo.com>
2023-01-18 18:36:46 -0600

    department table

--- db/schema.sql
+++ db/schema.sql
@@ -2,6 +2,10 @@ DROP DATABASE IF EXISTS employee_db;
 CREATE DATABASE employee_db;
 use employee_db;
 
-
+CREATE TABLE department (
+    id INT AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL
+    PRIMARY KEY (id)
+);
 
 
