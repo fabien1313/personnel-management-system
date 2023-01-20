a8eb5a63aabb241e72871b249da93378b235b78a
fabien moreno <fabienmoreno1331@yahoo.com>
2023-01-20 17:35:50 -0600

    resetting gitignore

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
 const cTable = require('console.table');
+
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database:
+
+});
