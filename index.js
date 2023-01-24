c5b58a9245af4bd4863b7985c97e9793435b0dc1
fabien moreno <fabienmoreno1331@yahoo.com>
2023-01-24 01:38:56 -0600

    established connection. Fixed env file

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
 const cTable = require('console.table');
+const dotenv = require('dotenv'); 
+require('dotenv').config();
 
 
 
-const connection = mysql.createConnection({
-    host: '',
-    user: '',
-    database: '',
-    password: '',
+
+const db_connection = mysql.createConnection({
+host: process.env.HOST,
+user: process.env.DB_USER,
+password: process.env.DB_PASSWORD,
+database: process.env.DATABASE
 });
+
+
+db_connection.connect(err => {
+    if (err) throw err;
+    console.log('You have successfully connected to the employee database!');
+
+});
+
+
+
+
+
+
+
+
+
