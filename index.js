1dc72d68d49380b90e01cec379593a3dd6ff45ab
fabien moreno <fabienmoreno1331@yahoo.com>
2023-01-27 09:14:15 -0600

    navmenu created

--- index.js
+++ index.js
@@ -18,11 +18,34 @@ database: process.env.DATABASE
 db_connection.connect(err => {
     if (err) throw err;
     console.log('You have successfully connected to the employee database!');
+    inquirer.prompt(questions)
 
 });
 
 
-
+const questions = [
+    {
+        type: 'list',
+        name: 'navmenu',
+        message: 'What would you like to do?',
+        choices: [
+         'View all departments',
+         'View all roles', 
+         'View all employees', 
+         'Add a department', 
+         'Add a role', 
+         'Add an employee', 
+         'update an employee role', 
+         'Update an employee manager',
+         'View employees by department',
+         'Delete a department',
+         'Delete a role',
+         'Delete an employee',
+         'View department budgets',
+         'Terminate program'
+        ]
+    }
+];
 
 
 
